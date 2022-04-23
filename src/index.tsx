577ac151b3184a967cae52170eb6497d3fdd0308
lotiko <l.despres@netc.fr>
2022-04-23 11:36:01 +0200

    start migration react typescript

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import 'core-js/features/array/flat-map'
+import 'core-js/features/map'
+import 'core-js/features/promise'
+import 'core-js/features/set'
+import 'raf/polyfill'
+import 'whatwg-fetch'
+import App from "./pages/app"
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('app-root'),
+)   
---
+++ src/pages/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+const App = () =>  {
+
+        return(
+            <>
+            <h1>421</h1>
+            </>
+        );
+}
+
+export default App;
