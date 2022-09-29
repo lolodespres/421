94eedfb9004879c6996991c0f60e541a2eee3802
lotiko <l.despres@netc.fr>
2022-09-29 10:57:36 +0200

    add dotenv and change default  port for heroku deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const accepts = require("accepts");
+require('dotenv').config()
 const express = require("express");
 const app = express();
-const port = 8001;
+const port = process.env.PORT || 80;
 app.use(express.static("public"));
 app.use("/favicon.ico", express.static("public/images/favicon.ico"));
 app.listen(port, () => {
